6d9be2b0f6be8f82edd21475135ddf0f8c633ea4
Aisha-Jamal <jamalaisha08@gmail.com>
2021-05-25 16:03:10 +0500

    Update App.js
    Updated App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br />
           Hello World from Aisha
         </p>
         <a
